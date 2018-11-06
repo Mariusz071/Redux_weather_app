86893f9e0da3c99cf2cef91c78fe4e251325f48e
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-11-06 10:12:38 +0000

    created weather list container and rendering city name into a table

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
+
+// importing function from reducer_weather file and assigning it to WeatherReducer
 import WeatherReducer from './reducer_weather';
 
 const rootReducer = combineReducers({
